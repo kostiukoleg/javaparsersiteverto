dc0af09816ca6cc3d6c91fd8a02ed17221c91a91
kostiukoleg <olegkostiuk@ukr.net>
2023-11-05 22:09:14 +0000

    Create new Category Constructor

--- src/main/java/com/kov/javalessons/Category.java
+++ src/main/java/com/kov/javalessons/Category.java
@@ -9,67 +9,49 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
-/**
- *
- * @author olegk
- */
 public class Category {
-   private String url;
-   private String image;
-   private String name;
-   
-   List<Category> categories = new ArrayList<>();
-   
-   public List<Category> GetText(Document doc, String selector){
-        Elements items = doc.select(selector);
-        for (Element item : items){
-            Category category = new Category();
-            category.setName(item.text());
-            categories.add(category);
+    private String url;
+    private String image;
+    private String name;
+    private boolean INSTANCE = false;
+    private List<Category> categories;
+ 
+    public Category (String url, String image, String name) {
+        this.url = url;
+        this.image = image;
+        this.name = name;
+    }
+    public Category () {
+        if(this.INSTANCE == false){
+            this.categories = new ArrayList<>();
+            System.out.println("Creating Category");
         }
-        return categories;
+        this.INSTANCE = true;
     }
    
-    public List<Category> GetHTML(Document doc, HashMap<String, String> selector){
+//    public List<Category> GetText(Document doc, String selector){
+//        Elements items = doc.select(selector);
+//        for (Element item : items){
+//            Category category = new Category();
+//            category.setName(item.text());
+//            categories.add(category);
+//        }
+//        return categories;
+//    }
+   
+    public List<Category> GetListCategories(Document doc, HashMap<String, String> selector){
         String sel = selector.get("url");
         Elements items = doc.select(sel);
         for (Element item : items){
-            Category category = new Category();
             String catURL = item.attr("href");
-            category.setURL(catURL);
             String catIMG = item.select("img").attr("src");
-            category.setImage(catIMG);
-            String clearName = RegexReplaceAll(item.select("h3").html(),"\s<mark class=\"count\">.+</mark>","");
-            category.setName(clearName);
+            String clearName = RegexReplaceAll(item.select("h3").html(),"\\s<mark class=\"count\">.+</mark>",""); 
+            Category category = new Category(catURL, catIMG, clearName);
             categories.add(category);
         }
         return categories;
     }
    
-    public String getURL(){
-        return url;
-    }
-    
-    public void setURL(String url){
-        this.url = url;
-    }
-    
-    public String getImage(){
-        return image;
-    }
-    
-    public void setImage(String image){
-        this.image = image;
-    }
-    
-    public String getName(){
-        return name;
-    }
-    
-    public void setName(String name){
-        this.name = name;
-    }
-    
     @Override
     public String toString(){
         return "{ \"url\":\""+url+"\", "
@@ -83,7 +65,7 @@ public class Category {
         selector.put("image", "li.product-category a img");
         selector.put("name", "li.product-category a h3");
         Document doc = JsoupConnect("https://verto-doors.com/ru/product-category/doors/");
-        List<Category> res = this.GetHTML(doc,selector);
+        List<Category> res = this.GetListCategories(doc,selector);
         return res;
     }
 }
